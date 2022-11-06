912c98141106f34f19a7b2bd1b856585c02d332c
Sharonusesopensource <105223828+Sharonusesopensource@users.noreply.github.com>
2022-11-06 18:31:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My_Favourite_Books_
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xb2agq)
---
+++ src/components/homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './homepage.css'
+
+export default function Home(){
+  return(
+    <div className="bkgrd">
+      <h1>Sharon's Favourite Books</h1>
+
+    </div>
+  )
+};
